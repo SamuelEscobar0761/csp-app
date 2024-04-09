f9494cdcccb5d1a19658276a2039471a988ba49f
Samuel Escobar <samueb8@gmail.com>
2024-04-09 12:23:48 -0400

    arreglando navbar y redirecciones

--- src/app/pages/RestaurantPage.tsx
+++ src/app/pages/RestaurantPage.tsx
@@ -20,14 +20,23 @@ export const RestaurantPage = () => {
     return(
         <div>
             <ImageSlider images={imagesCarousel} text={t('restaurant_page.restaurant_title')}/>
+            <br id="weekly"/>
+            <br/>
+            <br/>
             <h2 className="py-10 text-primary bg-white text-center text-7xl font-bold`">{t('restaurant_page.weekly_menu_title')}</h2>
             <div className="flex justify-center ...">
                 <iframe src='\assets\pdfs\comunicado.pdf' className='w-11/12 md:h-screen'/>
             </div>
+            <br id="main"/>
+            <br/>
+            <br/>
             <h2 className="py-10 text-primary bg-white text-center text-7xl font-bold`">{t('restaurant_page.main_menu_title')}</h2>
             <div className="flex justify-center ...">
                 <iframe src='\assets\pdfs\comunicado.pdf' className='w-11/12 md:h-screen'/>
             </div>
+            <br id="snack"/>
+            <br/>
+            <br/>
             <h2 className="py-10 text-primary bg-white text-center text-7xl font-bold`">{t('restaurant_page.snack_menu_title')}</h2>
             <div className="flex justify-center ...">
                 <iframe src='\assets\pdfs\comunicado.pdf' className='w-11/12 md:h-screen'/>
