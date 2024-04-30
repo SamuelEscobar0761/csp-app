438890f8eed7b4f7f94bdb18af45e13be88248be
Samuel Escobar <samueb8@gmail.com>
2024-04-30 10:02:41 -0400

    restaurant page con firebase

--- src/app/pages/RestaurantPage.tsx
+++ src/app/pages/RestaurantPage.tsx
@@ -3,9 +3,13 @@ import { ImageSlider } from "../components/ImageSlider";
 import { useEffect, useState } from "react";
 import Image from '../interfaces/Image';
 import LocateImageService from "../services/LocateImageService";
+import { getUrl, obtenerUrlImagenes } from "../services/FirebaseService";
 
 export const RestaurantPage = () => {
     const [imagesCarousel, setImagesCarousel] = useState<Image[]>([]);
+    const [weeklyMenus, setWeeklyMenus] = useState<Image[]>([]);
+    const [mainMenus, setMainMenus] = useState<Image[]>([]);
+    const [snackMenus, setSnackMenus] = useState<Image[]>([]);
     const { t } = useTranslation('ns1');
 
     useEffect(() => {
@@ -16,6 +20,57 @@ export const RestaurantPage = () => {
             .catch(error => {
                 console.error('Error al obtener las imágenes:', error);
         });
+        const loadWeeklyMenu = async () => {
+            try {
+                const weeklyMenuImages = await obtenerUrlImagenes("restaurant_page", "weekly_menu");
+        
+                    // Obtener URLs para cada imagen usando el path de cada objeto Image
+                    const weeklyMenuImagesWithUrl = await Promise.all(
+                        weeklyMenuImages.map(async (image) => ({
+                        ...image,
+                        url: await getUrl(image.path) // Obtener la URL real y añadirla al objeto
+                    }))
+                );
+                setWeeklyMenus(weeklyMenuImagesWithUrl);
+            } catch (error) {
+                console.error('Error al obtener las imágenes:', error);
+            }
+        }
+        const loadMainMenu = async () => {
+            try {
+                const mainMenuImages = await obtenerUrlImagenes("restaurant_page", "main_menu");
+        
+                    // Obtener URLs para cada imagen usando el path de cada objeto Image
+                    const mainMenuImagesWithUrl = await Promise.all(
+                        mainMenuImages.map(async (image) => ({
+                        ...image,
+                        url: await getUrl(image.path) // Obtener la URL real y añadirla al objeto
+                    }))
+                );
+                setMainMenus(mainMenuImagesWithUrl);
+            } catch (error) {
+                console.error('Error al obtener las imágenes:', error);
+            }
+        }
+        const loadSnackMenu = async () => {
+            try {
+                const snackMenuImages = await obtenerUrlImagenes("restaurant_page", "snack_menu");
+        
+                    // Obtener URLs para cada imagen usando el path de cada objeto Image
+                    const snackMenuImagesWithUrl = await Promise.all(
+                        snackMenuImages.map(async (image) => ({
+                        ...image,
+                        url: await getUrl(image.path) // Obtener la URL real y añadirla al objeto
+                    }))
+                );
+                setSnackMenus(snackMenuImagesWithUrl);
+            } catch (error) {
+                console.error('Error al obtener las imágenes:', error);
+            }
+        }
+        loadWeeklyMenu();
+        loadMainMenu();
+        loadSnackMenu();
     }, []);
     return(
         <div>
@@ -25,21 +80,27 @@ export const RestaurantPage = () => {
             <br/>
             <h2 className="py-10 text-primary bg-white text-center text-7xl font-bold`">{t('restaurant_page.weekly_menu_title')}</h2>
             <div className="flex justify-center ...">
-                <iframe src='\assets\pdfs\comunicado.pdf' className='w-11/12 md:h-screen'/>
+                {weeklyMenus.map((item, index) => (
+                    <img key={index} src={item.url!} className='w-2/3'/>
+                ))}
             </div>
             <br id="main"/>
             <br/>
             <br/>
             <h2 className="py-10 text-primary bg-white text-center text-7xl font-bold`">{t('restaurant_page.main_menu_title')}</h2>
             <div className="flex justify-center ...">
-                <iframe src='\assets\pdfs\comunicado.pdf' className='w-11/12 md:h-screen'/>
+                {mainMenus.map((item, index) => (
+                    <img key={index} src={item.url!} className='w-2/3'/>
+                ))}
             </div>
             <br id="snack"/>
             <br/>
             <br/>
             <h2 className="py-10 text-primary bg-white text-center text-7xl font-bold`">{t('restaurant_page.snack_menu_title')}</h2>
             <div className="flex justify-center ...">
-                <iframe src='\assets\pdfs\comunicado.pdf' className='w-11/12 md:h-screen'/>
+                {snackMenus.map((item, index) => (
+                    <img key={index} src={item.url!} className='w-2/3'/>
+                ))}
             </div>
         </div>
     );   
