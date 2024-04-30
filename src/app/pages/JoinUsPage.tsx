f929bb69036b6fb0a66922746b9ec9da7eb13f44
Samuel Escobar <samueb8@gmail.com>
2024-04-30 09:51:17 -0400

    join us page con firebase

--- src/app/pages/JoinUsPage.tsx
+++ src/app/pages/JoinUsPage.tsx
@@ -3,9 +3,12 @@ import Image from '../interfaces/Image';
 import { useTranslation } from "react-i18next";
 import LocateImageService from "../services/LocateImageService";
 import { ImageSlider } from "../components/ImageSlider";
+import { getUrl, obtenerUrlImagenes } from "../services/FirebaseService";
 
 export const JoinUsPage = () => {
     const [imagesCarousel, setImagesCarousel] = useState<Image[]>([]);
+    const [tariffImages, setTariffImages] = useState<Image[]>([]);
+    const [participationImages, setParticipationImages] = useState<Image[]>([]);
     const { t } = useTranslation('ns1');
 
     useEffect(() => {
@@ -16,6 +19,40 @@ export const JoinUsPage = () => {
             .catch(error => {
                 console.error('Error al obtener las imágenes:', error);
         });
+        const loadTariffImages = async () => {
+            try {
+                const tariffImages = await obtenerUrlImagenes("join_us_page", "tariff");
+        
+                    // Obtener URLs para cada imagen usando el path de cada objeto Image
+                    const tariffImagesWithUrl = await Promise.all(
+                        tariffImages.map(async (image) => ({
+                        ...image,
+                        url: await getUrl(image.path) // Obtener la URL real y añadirla al objeto
+                    }))
+                );
+                setTariffImages(tariffImagesWithUrl);
+            } catch (error) {
+                console.error('Error al obtener las imágenes:', error);
+            }
+        }
+        const loadParticipationImages = async () => {
+            try {
+                const participationImages = await obtenerUrlImagenes("join_us_page", "participation");
+        
+                    // Obtener URLs para cada imagen usando el path de cada objeto Image
+                    const participationImagesWithUrl = await Promise.all(
+                        participationImages.map(async (image) => ({
+                        ...image,
+                        url: await getUrl(image.path) // Obtener la URL real y añadirla al objeto
+                    }))
+                );
+                setParticipationImages(participationImagesWithUrl);
+            } catch (error) {
+                console.error('Error al obtener las imágenes:', error);
+            }
+        }
+        loadTariffImages();
+        loadParticipationImages();
     }, []);
     
     return(
@@ -26,14 +63,18 @@ export const JoinUsPage = () => {
             <br/>
             <h2 className="py-10 text-primary bg-white text-center text-7xl font-bold`">{t('join_us_page.tariff_title')}</h2>
             <div className="flex justify-center ...">
-                <img src='\assets\images\sin_foto.jpg' className='w-2/3'/>
+                {tariffImages.map((item, index) => (
+                    <img key={index} src={item.url!} className='w-2/3'/>
+                ))}
             </div>
             <br id='participation'/>
             <br/>
             <br/>
             <h2 className="py-10 text-primary bg-white text-center text-7xl font-bold`">{t('join_us_page.participation_title')}</h2>
             <div className="flex justify-center ...">
-                <img src='\assets\images\sin_foto.jpg' className='w-2/3'/>
+                {participationImages.map((item, index) => (
+                    <img key={index} src={item.url!} className='w-2/3'/>
+                ))}
             </div>
         </div>
     );
