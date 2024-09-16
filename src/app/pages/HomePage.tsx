39810768efc0584e2e5d648d3ef5b903b476833a
Samuel Escobar <samueb8@gmail.com>
2024-09-15 22:54:16 -0400

    Modificar la imágen de inicio y agregar la imágen del qr

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -36,15 +36,19 @@ export const HomePage = () => {
     
 
     return (
-        <div>
+        <div className='space-y-5 md:space-y-10'>
             <ImageSlider images={carouselImages} text='Club Social Petrolero' homepage={true}/>
             {info_images.map((item, index) => (
-                item.url ? <img key={index} src={item.url} className='my-10 w-full lg:h-96' alt="Club Image" /> : null
+                item.url ? <img key={index} src={item.url} className='w-full object-cover' alt="Club Image" /> : null
             ))}
             <h2 className='text-primary text-xl md:text-3xl lg:text-5xl text-center'>La mejor ubicación en la ciudad de La Paz</h2>
             <div className='flex justify-center'>
-                <img src='/assets/images/maps/maps_csp.jpg' className='w-full md:w-2/3 my-2 md:my-10' alt='Club location'/>
+                <img src='/assets/images/maps/maps_csp.jpg' className='w-full md:w-2/3 object-scale-down' alt='Club location'/>
             </div>
+            <div className='flex justify-center'>
+                <img src='/assets/images/qr/qr.jpg' className='w-full md:w-1/2 object-scale-down' alt='Código QR'/>
+            </div>
+            
         </div>
     );
 }
